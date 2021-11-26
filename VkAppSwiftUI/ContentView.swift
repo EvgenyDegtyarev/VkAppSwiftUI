6887f01986acdf4d0c7b20b05fc160130f92072b
EvgenyDegtyarev <e.degtyarev@icloud.com>
2021-11-26 12:01:04 +0300

    Initial Commit

---
+++ VkAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VkAppSwiftUI
+//
+//  Created by Евгений Дегтярев on 26.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VkAppSwiftUI/VkAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VkAppSwiftUIApp.swift
+//  VkAppSwiftUI
+//
+//  Created by Евгений Дегтярев on 26.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct VkAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
